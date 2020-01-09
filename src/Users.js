15cc5635097cb483a6f445067c65302e26ec863e
D191375 <seedevk8s@gmail.com>
2020-01-09 13:32:30 +0900

    https://jsonplaceholder.typicode.com/users 데이터 가져오기<--버튼 추가

--- src/Users.js
+++ src/Users.js
@@ -5,26 +5,27 @@ function Users() {
     const [users, setUsers] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const fetchUsers = async () => {
+        try {
+            setError(null);
+            setUsers(null);
+            setLoading(true);
+
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users/'
+            );
+
+            setUsers(response.data);
+
+        } catch (e) {
+            console.log(e.response.status);
+            setError(e);
+        }
+        setLoading(false);
+    }
     
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                setError(null);
-                setUsers(null);
-                setLoading(true);
-
-                const response = await axios.get(
-                    'https://jsonplaceholder.typicode.com/users'
-                );
-
-                setUsers(response.data);
-
-            } catch (e) {
-                setError(e);
-            }
-            setLoading(false);
-        }
-
         fetchUsers();
 
     },[]);
@@ -35,11 +36,17 @@ function Users() {
     if (!users) return null;
 
 
-    return <ul>
-        {users.map(user => <li key={user.id}>
-            {user.username} ({user.name})
-        </li>)}
-    </ul>
+    return (
+        <>
+            <ul>
+                {users.map(user => <li key={user.id}>
+                    {user.username} ({user.name})
+                </li>)}
+            </ul>
+            <button onClick={fetchUsers}>다시 불러오기</button>
+        </>
+    );
+
 }
 
 export default Users;
