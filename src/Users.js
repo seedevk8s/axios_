ee7449e4daea7bd269705234658f14af3400e01d
D191375 <seedevk8s@gmail.com>
2020-01-09 13:16:09 +0900

    https://jsonplaceholder.typicode.com/users 데이터 가져오기

--- src/Users.js
+++ src/Users.js
@@ -30,11 +30,16 @@ function Users() {
     },[]);
 
 
-    return (
-        <div>
-            
-        </div>
-    );
+    if (loading) return <div>로딩중...</div>
+    if (error) return <div>에러가 발생했습니다.</div>
+    if (!users) return null;
+
+
+    return <ul>
+        {users.map(user => <li key={user.id}>
+            {user.username} ({user.name})
+        </li>)}
+    </ul>
 }
 
 export default Users;
