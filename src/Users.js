c80716bffea5020a8730656dd8e8247f8681b410
D191375 <seedevk8s@gmail.com>
2020-01-09 15:41:56 +0900

    loading 에러가 났음<=복붙함

--- src/Users.js
+++ src/Users.js
@@ -1,77 +1,36 @@
-import React, { useEffect, useReducer } from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import useAsync from "./useAsync";
 
-// loading, success, error 총 3가지 action.
-function reducer(state, action) {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null
-            };
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.data,
-                error: null
-            };
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.error
-            };
-        default:
-            throw new Error(`Unhandled action type: ${action.type}`);
-    }
+// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
+// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
+async function getUsers() {
+    const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+    return response.data;
 }
 
 function Users() {
+    const [state, refetch] = useAsync(getUsers, []);
 
-    const [state, dispatch] = useReducer(reducer,{
-        loading: false,
-        data: null,
-        error: null,
-    });
+    const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
 
-    const fetchUsers = async () => {
-        dispatch({ type: 'LOADING' });
-
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users/'
-            );
-        dispatch({ type: 'SUCCESS', data: response.data })
-
-        } catch (e) {
-            dispatch({ type: 'ERROR', error: e});
-        }
-    }
-    
-    useEffect(() => {
-        fetchUsers();
-
-    },[]);
-
-
-    const { loading, data: users, error } = state;
-    if (loading) return <div>로딩중...</div>
-    if (error) return <div>에러가 발생했습니다.</div>
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
     if (!users) return null;
-
-
     return (
         <>
             <ul>
-                {users.map(user => <li key={user.id}>
-                    {user.username} ({user.name})
-                </li>)}
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
             </ul>
-            <button onClick={fetchUsers}>다시 불러오기</button>
+            <button onClick={refetch}>다시 불러오기</button>
         </>
     );
-
 }
 
 export default Users;
--- src/useAsync.js
+++ src/useAsync.js
@@ -1,6 +1,5 @@
-import { useReducer, useEffect, useCallback } from 'react';
+import { useReducer, useEffect } from 'react';
 
-// loading, success, error 총 3가지 action.
 function reducer(state, action) {
     switch (action.type) {
         case 'LOADING':
@@ -26,54 +25,30 @@ function reducer(state, action) {
     }
 }
 
-export  default function useAsync(callback, deps = []) {
+function useAsync(callback, deps) {
     const [state, dispatch] = useReducer(reducer, {
         loading: false,
         data: null,
-        error: null
+        error: false
     });
 
-    const fetchData = useCallback(async () => {
-        dispatch({ type: 'LOADING '});
-
+    const fetchData = async () => {
+        dispatch({ type: 'LOADING' });
         try {
             const data = await callback();
-            dispatch( {type: 'SUCCESS', data });
-        }catch (e) {
-            dispatch( {type: 'ERROR', error: e});
+            dispatch({ type: 'SUCCESS', data });
+        } catch (e) {
+            dispatch({ type: 'ERROR', error: e });
         }
-    }, [callback]);
+    };
 
     useEffect(() => {
         fetchData();
-
+        // eslint 설정을 다음 줄에서만 비활성화
         // eslint-disable-next-line
     }, deps);
 
     return [state, fetchData];
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default useAsync;
